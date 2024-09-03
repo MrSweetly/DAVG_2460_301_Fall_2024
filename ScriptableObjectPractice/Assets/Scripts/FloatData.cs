91aa813fb6d2abde82a0fb5e9dbd05fe34a1d5ca
Brandon Gomez <brandon.gomez.alex@gmail.com>
2024-09-02 22:37:02 -0600

    Added UnityEvents to ScriptableObjectPractice

--- ScriptableObjectPractice/Assets/Scripts/FloatData.cs
+++ ScriptableObjectPractice/Assets/Scripts/FloatData.cs
@@ -5,6 +5,11 @@ public class FloatData : ScriptableObject
 {
    public float value;
 
+   public void SetValue(float number)
+   {
+      value = number;
+   }
+   
    public void UpdateValue(float number)
    {
       value += number;
