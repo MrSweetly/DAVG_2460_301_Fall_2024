7b7f91e74246e70d57399124ee186affd737d482
Brandon Gomez <brandon.gomez.alex@gmail.com>
2024-09-23 13:26:10 -0600

    Updated MatchingGame Challenge 2
    Fixed issues. So happy.

--- MatchingGame/Assets/Scripts/Game/MeshBehavior.cs
+++ MatchingGame/Assets/Scripts/Game/MeshBehavior.cs
@@ -9,17 +9,9 @@ public class MeshBehavior : MonoBehaviour
     {
         rendererObj = GetComponent<MeshRenderer>();
         
-        if (rendererObj == null)
-        {
-            rendererObj = gameObject.AddComponent<MeshRenderer>();
-        }
+        if (rendererObj == null) {
+            rendererObj = gameObject.AddComponent<MeshRenderer>(); }
     }
-    
-    private void Start()
-    {
-        rendererObj = GetComponent<MeshRenderer>();
-    }
-
     public void ChangeRendererColor(ColorID obj)
     {
         rendererObj.material.color = obj.value;
